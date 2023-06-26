3d6d9b61a87e21040baf2fff933c869ddfbf04dd
Dusky-FOX-y <mr.dusky.fox@gmail.com>
2023-06-26 21:44:06 +0300

    hui

--- js/cafe.js
+++ js/cafe.js
@@ -289,7 +289,7 @@
       $('.js-status').removeClass('shown');
     },
     apiRequest: function(method, data, onCallback) {
-      window.Telegram.WebApp.sendData(data);
+      window.Telegram.WebApp.sendData("fdfdfdf");
       onCallback();
       // var authData = Telegram.WebApp.initDataRaw || '';
       // $.ajax(Cafe.apiUrl, {
